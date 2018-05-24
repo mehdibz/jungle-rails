eb226feea24d7d72dfc82700bf792fe1f127c05c
mehdibz <bz.mehdi@gmail.com>
2018-05-24 21:48:13 +0000

    Soldout-Badge Complete

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ActiveRecord::Base
+
+  monetize :price_cents, numericality: true
+  mount_uploader :image, ProductImageUploader
+
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :category, presence: true
+
+  def sold_out?
+  	quantity == 0
+  end
+end
